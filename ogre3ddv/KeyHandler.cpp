80c4d369db2c98d6c3a4aa97e097591e04f13b62
potionsnotusedlol <mskomissarov07@gmail.com>
2025-11-20 00:49:43 +0300

    this shii gon kill me someday

--- ogre3ddv/KeyHandler.cpp
+++
@@ -1,7 +0,0 @@
-#include "KeyHandler.h"
-
-bool KeyHandler::keyPressed(const OgreBites::KeyboardEvent& event) {
-    if (event.keysym.sym == OgreBites::SDLK_ESCAPE) Ogre::Root::getSingleton().queueEndRendering();
-
-    return true;
-}
