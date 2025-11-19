01763b0113db10c48f1d13da3f1b4dee2f8c10bf
potionsnotusedlol <mskomissarov07@gmail.com>
2025-11-20 00:17:54 +0300

    removed all the shit plechov vibecoded

--- ogre3ddv/KeyHandler.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Ogre.h"
-#include "OgreApplicationContext.h"
-
-class KeyHandler : public OgreBites::InputListener { bool keyPressed(const OgreBites::KeyboardEvent& event) override; };
